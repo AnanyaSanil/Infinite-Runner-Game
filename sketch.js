0aaa8500a81d727d48de0b0a531d40224b656c33
AnanyaSanil <72144552+AnanyaSanil@users.noreply.github.com>
2021-02-24 10:39:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,212 +2,164 @@ var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
 
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
+var doraemon,doraemon_running;
+var back,backImg,invisibleGround;
+var doraCake,doraCakeImg,car,carImg;
+var cakeGroup,carGroup;
+var gameOver,gameOverImg;
+var restart,restartImg;
 
-var cloudsGroup, cloudImage;
-var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4; 
 var score;
-var gameOverImg,restartImg
-
 
 function preload(){
-  trex_running = loadAnimation("trex_1.png","trex_2.png","trex_3.png");
-  trex_collided = loadAnimation("trex_collided.png");
+doraemon_running = loadAnimation("Doraemon 1.png","Doraemon 2.png","Doraemon 3.png","Doraemon 4.png","Doraemon 5.png","Doraemon 6.png");
+  //doraemon_fallen = loadAnimation("doraemon_fallen.png")
+backImg = loadImage("background2.png");
   
-  groundImage = loadImage("ground.png");
+doraCakeImg = loadImage("doracake.png");
+carImg = loadImage("police_car.png"); 
   
-  cloudImage = loadImage("cloud.png");
-  
-  obstacle1 = loadImage("obstacle1.png");
-  obstacle2 = loadImage("obstacle2.png");
-  obstacle3 = loadImage("obstacle3.png");
-  obstacle4 = loadImage("obstacle4.png");
-  
-  
-  restartImg = loadImage("restart.png")
-  gameOverImg = loadImage("gameOver.png")
-  
-
+  gameOverImg = loadImage("gameover3.png");
+  restartImg = loadImage("restart.png");
 }
-
 function setup() {
-  createCanvas(750, 360);
-     var message = "hello";
-  console.log(message)
-  
-  trex = createSprite(50,180,20,50);
-  
-  trex.addAnimation("running", trex_running);
-  trex.addAnimation("collided", trex_collided);
-  trex.scale = 0.1;
-  
-  ground = createSprite(200,260,900,100);
-  ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
+  createCanvas(600, 600);
   
   
-  gameOver = createSprite(300,100);
+  back = createSprite(600,300,400,20);
+  back.addImage(backImg);
+  back.scale = 1.2;
+
+  doraemon = createSprite(80,490,20,50);
+  doraemon.addAnimation("running",doraemon_running);
+  doraemon.scale = 0.9;
+  
+  gameOver = createSprite(300,150);
   gameOver.addImage(gameOverImg);
   
-  restart = createSprite(300,100);
+  restart = createSprite(300,300);
   restart.addImage(restartImg);
   
-  gameOver.scale = 0.1;
-  restart.scale = 0.1;
+  gameOver.scale = 0.3;
+  restart.scale = 1.2;
   
-  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround = createSprite(200,510,800,10);
   invisibleGround.visible = false;
-  trex.collide(invisibleGround)
   
-  //create Obstacle and Cloud Groups
-  obstaclesGroup = createGroup();
-  cloudsGroup = createGroup();
-
+  cakeGroup = createGroup();
+  carGroup = createGroup();
   
-  trex.setCollider("rectangle",0,0,500,500, 500);
-  trex.debug = false
+//  doraemon.setCollider("rectangle",0,0,doraemon.width,doraemon.height);
+  doraemon.debug = false;
   
   score = 0;
-  
 }
 
 function draw() {
+   background(255);
+back.velocityX = -5;
   
-  background(180);
-  //displaying score
-  text("Score: "+ score, 500,50);
-    
   
   if(gameState === PLAY){
-    //move the 
+    
     gameOver.visible = false;
     restart.visible = false;
-    //change the trex animation
-      trex.changeAnimation("running", trex_running);
     
-    ground.velocityX = -(4 + 3* score/100)
-    //scoring
-    score = score + Math.round(getFrameRate() /60);
-    
-    if(score>0 && score%100 === 0){
-       
-    }
-    
-    if (ground.x < 0){
-      ground.x = ground.width/2;
-    }
-    
-    //jump when the space key is pressed
-    if(keyDown("space")&& trex.y >= 100) {
-        trex.velocityY = -12;
-    }
-    
-    //add gravity
-    trex.velocityY = trex.velocityY + 0.8
+  back.velocityX = -(4 + 3* score/100);
   
-    //spawn the clouds
-    spawnClouds();
   
-    //spawn obstacles on the ground
-    spawnObstacles();
-    if(obstaclesGroup.isTouching(trex)){
-        //trex.velocityY = -12;
-        
-        gameState = END;
-      
-    }
+  if(back.x < 0){
+    back.x = back.width/2;
   }
-   else if (gameState === END) {
-      gameOver.visible = true;
-      restart.visible = true;
-     //change the trex animation
-      trex.changeAnimation("collided", trex_collided);
-       
-
-     
-      ground.velocityX = 0;
-      trex.velocityY = 0
-      
-     
-      //set lifetime of the game objects so that they are never destroyed
-    obstaclesGroup.setLifetimeEach(-1);
-    cloudsGroup.setLifetimeEach(-1);
-     
-     obstaclesGroup.setVelocityXEach(0);
-     cloudsGroup.setVelocityXEach(0);    
-     
-     if(mousePressedOver(restart)){
-  // console.log("restart the game") 
-    reset();
+  
+  if(keyDown("space") && doraemon.y >= 300){
+    doraemon.velocityY = -15;
+  }
     
-  }
-     
+  doraemon.velocityY = doraemon.velocityY + 1;
+  
+    //monkey.collide(ground);
+  cake();
+  car();
+  
+    if(carGroup.isTouching(doraemon)) {
+   gameState = END; 
+    }
+      if(cakeGroup.isTouching(doraemon)){
+      cakeGroup.destroyEach();
+        score = score+2;
+      }
    }
   
- 
-  //stop trex from falling down
-  trex.collide(invisibleGround);
+  else if(gameState === END){
+    
+   //doraemon.changeAnimation("fallen",doraemon_fallen);
+    
+    gameOver.visible = true;
+      restart.visible = true;
+     
+    if(mousePressedOver(restart)) {
+      reset();
+    }
+    
+    back.velocityX = 0;
+    doraemon.velocityY = 0; 
+    
+    carGroup.setLifetimeEach(-1);
+    cakeGroup.setLifetimeEach(-1);
+    
+     carGroup.setVelocityXEach(0);
+     cakeGroup.setVelocityXEach(0); 
   
-
+    }
+  
+  
+  doraemon.collide(invisibleGround);
+  
   drawSprites();
   
+  stroke("black");
+  textSize(20);
+  fill("black");
+  textFont("algerian");
+  text("SCORE : "+score,250,50);
+ 
+
+}
+function cake(){
+  if(World.frameCount % 80 === 0){
+   var cake = createSprite(550,500,10,10);
+    cake.y = Math.round(random(100,450));
+    cake.addImage(doraCakeImg);
+    cake.scale = 0.06;
+    cake.velocityX = -6;
+    
+    cake.lifetime = 100;
+    
+    cake.depth = doraemon.depth;
+    doraemon.depth = doraemon.depth+1;
+  
+    cakeGroup.add(cake);
+  
+  }
+}
+function car(){
+  if(World.frameCount % 100 === 0){  
+  var car = createSprite(400,470,10,10);
+  car.addImage(carImg);
+  car.velocityX = -(6 + score/100);
+  car.scale = 0.9;
+  car.lifetime = 100;
+  
+  carGroup.add(car);
+}
 }
 function reset(){
   gameState = PLAY;
   gameOver.visible = false;
-  restart.visible = false
-  cloudsGroup.destroyEach();
-  obstaclesGroup.destroyEach();
- trex.changeAnimation("running", trex_running)
+  restart.visible = false;
+  cakeGroup.destroyEach();
+  carGroup.destroyEach();
+  //doraemon.changeAnimation("running",doraemon_running);
   score = 0;
-  
 }
-
-
-
-function spawnObstacles(){
- if (frameCount % 60 === 0){
-   var obstacle = createSprite(600,150,10,40);
-   obstacle.velocityX = -(6 + score/100);
-    //generate random obstacles
-    var rand = Math.round(random(1,2));
-    switch(rand) {
-      case 1: obstacle.addImage(obstacle1);
-              break;
-      case 2: obstacle.addImage(obstacle2);
-              break;
-      default: break;
-    }
-   
-    //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.5;
-    obstacle.lifetime = 300;
-      obstacle.scale = 0.5
-
-   //add each obstacle to the group
-    obstaclesGroup.add(obstacle);
- }
-}
-
-function spawnClouds() {
-  //write code here to spawn the clouds
- if (frameCount % 60 === 0) {
-    var cloud = createSprite(600,120,40,10);
-    cloud.y = Math.round(random(80,120));
-    cloud.addImage(cloudImage);
-    cloud.scale = 0.5;
-    cloud.velocityX = -3;
-    
-     //assign lifetime to the variable
-    cloud.lifetime = 200;
-    
-    //adjust the depth
-    cloud.depth = trex.depth;
-    trex.depth = trex.depth + 1;
-    
-    //add each cloud to the group
-    cloudsGroup.add(cloud);
-  }
-}
-
